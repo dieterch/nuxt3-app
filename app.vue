1eb2dfcbda2e201048a2bb6ac99433970d39d1b6
dieterch <dieter.chvatal@gmail.com>
2024-10-23 11:42:40 +0200

    move button to submenu

--- app.vue
+++ app.vue
@@ -1,15 +1,38 @@
 <template>
   <v-app>
 
-    <v-app-bar app color="primary" dark>
+    <v-app-bar 
+      app 
+      _color="primary" 
+      dark
+      name="menu-bar" 
+      density="compact"
+      color="grey-darken-3"
+      :elevation="5"
+      >
       <v-toolbar-title>Expenses</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn text to="/">Home</v-btn>
-      <v-btn text to="/dialogtrips">Trips</v-btn>
+      
+      <v-btn>
+        Setup
+        <v-menu activator="parent">
+          <v-list>
+            <v-list-item><v-btn text to="/dialogtrips">Trips</v-btn></v-list-item>
+            <v-list-item><v-btn text to="/dialogusers">Users</v-btn></v-list-item>
+            <v-list-item><v-btn text to="/dialogcategories">Categories</v-btn></v-list-item>
+            <v-list-item><v-btn text to="/dialogexpenses">Dialog Expenses</v-btn></v-list-item>
+            <v-list-item><v-btn text to="/expenses">Expenses</v-btn></v-list-item>
+          </v-list>
+        </v-menu>
+      </v-btn>
+
+      <!--v-btn text to="/dialogtrips">Trips</v-btn>
       <v-btn text to="/dialogusers">Users</v-btn>
       <v-btn text to="/dialogcategories">Categories</v-btn>
       <v-btn text to="/dialogexpenses">Dialog Expenses</v-btn>
-      <v-btn text to="/expenses">Expenses</v-btn>
+      <v-btn text to="/expenses">Expenses</v-btn-->
+
     </v-app-bar>
       
       <v-main>

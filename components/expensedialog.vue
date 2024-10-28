5579c39b2bfa744adc0892d2814fd52a976b0d8c
dieterch <dieter.chvatal@gmail.com>
2024-10-28 18:48:26 +0100

    component expensedialog.vue

---
+++ components/expensedialog.vue
@@ -0,0 +1,240 @@
+<template>
+    <v-dialog
+    v-model="dialog"
+    max-width="410"
+    >
+    <template v-slot:activator="{ props: activatorProps }">
+        <v-btn
+        class="text-none font-weight-regular"
+        prepend-icon="mdi-cash-register"
+        color="surface-variant"
+        rounded="0"
+        elevation="1"
+        text="Add Expense"
+        novariant="tonal"
+        v-bind="activatorProps"
+        ></v-btn>
+    </template>
+  
+        <!--v-card
+          prepend-icon="mdi-cash-register"
+          title="Expense"
+    -->
+        <v-card>
+        <v-card-title>
+            <v-sheet>
+                <v-icon icon="mdi-cash-register"></v-icon>
+                Expense - {{ props.selectedTrip.name }}       
+            </v-sheet>
+        </v-card-title> 
+        <v-card-text>
+            <v-form 
+                ref="expenseForm" 
+                v-model="isFormValid" 
+                lazy-validation
+            >
+            <v-row dense>
+                <!-- Description Input -->
+                <v-col
+                    cols="12"
+                    md="8"
+                    sm="12"
+                >
+                    <v-text-field
+                        density="compact"
+                        v-model="lformData.description"
+                        label="Title*"
+                        placeholder="brief description of the expense"
+                        required
+                        :rules="[v => !!v || 'Description is required']"
+                    ></v-text-field>
+                </v-col>
+  
+                <!-- Category Dropdown -->
+                <v-col
+                    cols="12"
+                    md="4"
+                    sm="6"
+                >
+                    <v-select
+                        density="compact"
+                        v-model="lformData.categoryId"
+                        :items="dialogcategories"
+                        item-title="name"
+                        item-value="id"
+                        label="♥"
+                        required
+                        :rules="[v => !!v || 'required']"
+                    ></v-select>
+                </v-col>
+
+                <!-- Currency Dropdown -->
+                <v-col
+                    cols="12"
+                    md="3"
+                    sm="4"
+                >
+                <v-select
+                  density="compact"
+                  v-model="lformData.currency"
+                  :items="currencies"
+                  item-title="symbol"
+                  item-value="symbol"
+                  nolabel="Currency"
+                  required
+                  :rules="[v => !!v || 'required']"
+                ></v-select>
+                </v-col>
+
+                <!-- Amount Input -->
+                <v-col
+                    cols="12"
+                    md="9"
+                    sm="12"
+                >
+                <v-text-field
+                density="compact"
+                v-model="lformData.amount"
+                type="number"
+                label="Amount"
+                required
+                :rules="[v => !!v || 'Amount is required']"
+                ></v-text-field>
+                </v-col>
+
+                <!-- User Dropdown -->  
+                <v-col
+                    cols="12"
+                    md="6"
+                    sm="12"
+                >
+                <v-select
+                  density="compact"
+                  v-model="lformData.userId"
+                  :items="props.selectedTrip.users"
+                  item-title="user.name"
+                  item-value="user.id"
+                  label="Select User"
+                  required
+                  :rules="[v => !!v || 'User is required']"
+                ></v-select>
+                </v-col>
+
+                <!-- Date Input -->  
+                <v-col
+                    cols="12"
+                    md="6"
+                    sm="12"
+                >
+                <v-date-input
+                    density="compact"
+                    v-model="lformData.date"
+                    label="Expense Date"
+                    required
+                    :rules="[v => !!v || 'Date is required']"
+                  ></v-date-input>
+                </v-col>  
+            </v-row>
+  
+            <small class="text-caption text-medium-emphasis">*indicates required field</small>
+            </v-form>
+          </v-card-text>
+  
+          <v-divider></v-divider>
+  
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text="Add" @click="submitExpense" :disabled="!isFormValid">Add</v-btn>
+            <v-btn text="Close" @click="closeDialog">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <!--pre>{{ props }}</pre-->
+</template>
+
+<script setup>
+    import { ref, onMounted, defineEmits, defineProps } from 'vue'
+    import { useFetch } from '#app'
+    import VueCookies from 'vue-cookies'
+
+    const props = defineProps(['selectedTrip']);
+    const emit = defineEmits(['refresh']);
+
+    const isFormValid = ref(false)
+    const dialog = ref(false)
+    const dialogcategories = ref([])
+    //const lselectedTrip = ref(null)
+
+    const lformData = ref({
+    amount: null,
+    currency: '€',
+    date: new Date(),
+    location: '',
+    categoryId: null,
+    description: '',
+    tripId: '',
+    userId: '',
+  })
+
+  // Reset the Form
+  const resetForm = async () => {
+    lformData.value.amount = null
+    lformData.value.date = new Date()
+    lformData.value.description = ''
+    lformData.value.categoryId = null  
+  }
+
+  //currencies seed => would make sense to transfer this table to the database.
+  const currencies = [
+    { name: 'USD', symbol: '$' },
+    { name: 'EUR', symbol: '€' },
+  ]
+
+    // Fetch Data on Mount
+    onMounted(async () => {
+
+    const { data: categoriesData } = await useFetch('/api/categories')
+    dialogcategories.value = categoriesData.value
+
+    // read selected Trip from cookiev...
+    //lselectedTrip.value = VueCookies.get('selectedTrip')
+    //console.log("selectedTrip: ",lselectedTrip.value)
+
+    }) 
+
+    //Submit the 'add expense' dialog content:
+    const submitExpense = async () => {
+        if (!isFormValid.value) return
+
+        //DEBUG: console.log('Submitted Data before:', formData.value)
+        // Add logic to submit the data via your API
+        lformData.value.tripId = props.selectedTrip.id
+        lformData.value.amount = parseFloat(lformData.value.amount)
+
+        //DEBUG: console.log('Submitted Data after:', formData.value)
+        
+        // Send data to API
+        try {
+        await $fetch('/api/expenses', {
+            method: 'POST',
+            body: lformData.value,
+        })
+
+        // Reset the form
+        resetForm()
+        emit('refresh')
+
+        // close the Dialog form
+        dialog.value = false
+        } catch (error) {
+            console.error('Error submitting form:', error)
+            alert(error)
+        }
+    }
+
+    // Close Dialog without Submission of data
+    const closeDialog = () => {
+        resetForm()
+        dialog.value = false
+    }
+</script>
--- pages/expenses.vue
+++ pages/expenses.vue
@@ -14,177 +14,66 @@
         return-object
       ></v-select>
 
-      <!--mydialog
-        :formData="formData"
-      ></mydialog-->
-
-      <!-- Add Expense Dialog -->
-      <v-dialog v-model="isDialogOpen" max-width="410">
-        <template #activator="{ props }">
-        <v-row justify="start">
-          <v-col>
-            <v-btn
-              v-bind="props"
-              color="surface-variant"
-              rounded="0"
-              elevation="1"
-              :disabled="!selectedTrip"
-            >
-              Add Expense
-            </v-btn>
-          </v-col>
-          <v-col>
-            <v-sheet>
-              {{ totalExpenses }}€ - {{ totalDays }} days<br>
-              {{ expensePerDay }}€ per day.<br>
-            </v-sheet>
-          </v-col>
-          <v-spacer></v-spacer>
-          <v-col class="text-right">
-            <v-btn
-              notext="Debug Trip"
-              rounded="0"
-              elevation="1"
-              color="surface"
-              size="x-small"
-              icon="mdi-bug"
-              @click="debug = !debug"
-            ></v-btn>        
-            <v-btn
-              notext="Test"
-              rounded="0"
-              color="surface"
-              elevation="1"
-              size="x-small"
-              icon="mdi-refresh"
-              @click="tripChanged"
-            ></v-btn>
-          </v-col>
-        </v-row>
-        </template>
-
-        <v-card>
-          <v-card-title>Add Expense - {{ selectedTrip.name }}</v-card-title>
-          <v-card-text>
- 
-          <v-form ref="expenseForm" v-model="isFormValid" lazy-validation>
-            <v-row>
-              <!-- Description Input -->
-              <v-col cols="12" md="8">
-                <v-text-field
-                  density="compact"
-                  v-model="formData.description"
-                  label="Title"
-                  placeholder="brief description of the expense"
-                  required
-                  :rules="[v => !!v || 'Description is required']"
-                ></v-text-field>
-              </v-col>
-              <!-- Category Dropdown -->
-              <v-col cols="12" md="4">
-                <v-select
-                  density="compact"
-                  v-model="formData.categoryId"
-                  :items="dialogcategories"
-                  item-title="name"
-                  item-value="id"
-                  label="♥"
-                  required
-                  :rules="[v => !!v || 'required']"
-                ></v-select>
-              </v-col>
-            </v-row>
-            <v-row>
-            <!-- Currency Dropdown -->
-            <v-col cols="12" md="3">
-                <v-select
-                  density="compact"
-                  v-model="formData.currency"
-                  :items="currencies"
-                  item-title="symbol"
-                  item-value="symbol"
-                  nolabel="Currency"
-                  required
-                  :rules="[v => !!v || 'required']"
-                ></v-select>
-              </v-col> 
-              <!-- Amount Input -->
-              <v-col cols="12" md="9">
-                <v-text-field
-                  density="compact"
-                  v-model="formData.amount"
-                  type="number"
-                  label="Amount"
-                  required
-                  :rules="[v => !!v || 'Amount is required']"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-
-              <!-- User Dropdown -->
-              <v-col cols="12" md="6">
-                <v-select
-                  density="compact"
-                  v-model="formData.userId"
-                  :items="selectedTrip.users"
-                  item-title="user.name"
-                  item-value="user.id"
-                  label="Select User"
-                  required
-                  :rules="[v => !!v || 'User is required']"
-                ></v-select>
-              </v-col>
-
-              <!-- Date Input -->
-              <v-col cols="12" md="6">
-                  <v-date-input
-                    density="compact"
-                    v-model="formData.date"
-                    label="Expense Date"
-                    required
-                    :rules="[v => !!v || 'Date is required']"
-                  ></v-date-input>
-              </v-col>
-            </v-row>
-          </v-form>
-            
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn text="Add" @click="submitExpense" :disabled="!isFormValid">Add</v-btn>
-            <v-btn text="Close" @click="closeDialog">Close</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-
       <v-row>
-            <v-col>
-                <v-data-table 
-                    :items="filteredexpenses"
-                    :headers="expense_headers"
-                    v-model:sort-by="sortBy"
-                    density="compact"
-                    hide-default-footer
-                    v-if="selectedTrip"
-                >
-                  <template v-slot:item.category.icon="{ item }">
-                    <v-icon :icon=item.category.icon></v-icon>
-                  </template>
-                  <template v-slot:item.actions="{ item }">
-                  <v-btn
-                    class="ma-2"
-                    rounded="0"
-                    size="x-small"
-                    nocolor="grey"
-                    elevation="1"
-                    @click="deleteExpense(item)"
-                    icon="mdi-delete"
-                  ></v-btn>
-                </template>            
-                </v-data-table>
-            </v-col>
-        </v-row>
+
+      <v-col>
+        <expensedialog 
+          :key="selectedTrip" 
+          :selectedTrip="selectedTrip" 
+          :v-bind="selectedTrip"
+          @refresh="tripChanged"
+        ></expensedialog>
+      </v-col>
+
+      <v-col class="text-right">
+      <v-btn
+        notext="Debug Trip"
+        rounded="0"
+        elevation="1"
+        color="surface"
+        size="x-small"
+        icon="mdi-bug"
+        @click="debug = !debug"
+      ></v-btn>        
+      <v-btn
+        notext="Test"
+        rounded="0"
+        color="surface"
+        elevation="1"
+        size="x-small"
+        icon="mdi-refresh"
+        @click="tripChanged"
+      ></v-btn>
+      </v-col>
+    </v-row>
+
+    <v-row>
+          <v-col>
+              <v-data-table 
+                  :items="filteredexpenses"
+                  :headers="expense_headers"
+                  v-model:sort-by="sortBy"
+                  density="compact"
+                  hide-default-footer
+                  v-if="selectedTrip"
+              >
+                <template v-slot:item.category.icon="{ item }">
+                  <v-icon :icon=item.category.icon></v-icon>
+                </template>
+                <template v-slot:item.actions="{ item }">
+                <v-btn
+                  class="ma-2"
+                  rounded="0"
+                  size="x-small"
+                  nocolor="grey"
+                  elevation="1"
+                  @click="deleteExpense(item)"
+                  icon="mdi-delete"
+                ></v-btn>
+              </template>            
+              </v-data-table>
+          </v-col>
+      </v-row>
       <div v-if="debug">
         Selected Trip:
         <pre>{{ selectedTrip }}</pre>
@@ -200,31 +89,7 @@
   import { useFetch } from '#app'
   import VueCookies from 'vue-cookies'
 
-  // Ref Variables
-  const formData = ref({
-    amount: null,
-    currency: '€',
-    date: new Date(),
-    location: '',
-    categoryId: null,
-    description: '',
-    tripId: '',
-    userId: '',
-  })
-  
-  // Reset the Form
-  const resetForm = async () => {
-    formData.value.amount = null
-    formData.value.date = new Date()
-    formData.value.description = ''
-    formData.value.categoryId = null  
-  }
-
-  const isFormValid = ref(false)
-  const isDialogOpen = ref(false)
   const dialogtrips = ref([])
-  const dialogUsers = ref({})
-  const dialogcategories = ref([])
   const selectedTrip = ref(null)
   const filteredexpenses = ref([])
   const debug = ref(false)
@@ -273,8 +138,8 @@
     const { data: tripsData } = await useFetch('/api/trips')
     dialogtrips.value = tripsData.value
     
-    const { data: categoriesData } = await useFetch('/api/categories')
-    dialogcategories.value = categoriesData.value
+    // const { data: categoriesData } = await useFetch('/api/categories')
+    // dialogcategories.value = categoriesData.value
     
     // read selected Trip from cookiev...
     selectedTrip.value = VueCookies.get('selectedTrip')
@@ -292,7 +157,7 @@
   //columns definition & optimization
   const expense_headers = [
     //format the date 
-    { title: 'Date', key: 'formateddate', width: "5%", value: item => new Date(item.date).toLocaleDateString(), sortable: "false", align: "end"},
+    { title: 'Date', key: 'formateddate', width: "5%", value: item => new Date(item.date).toLocaleDateString("de-CA", {year:"numeric", month: "2-digit", day:"2-digit"}), sortable: "false", align: "end"},
     // do not display the Trip Name:
     // { title: 'Trip', key: 'trip.name', sortable: "false"},
     { title: 'Cat', key: 'category.icon', width: "5%", align: "left" },
@@ -303,52 +168,6 @@
     //add a column for action buttons
     { title: 'Actions', key: 'actions', width: "5%", sortable: false },
   ]
-
-  //currencies seed => would make sense to transfer this table to the database.
-  const currencies = [
-    { name: 'USD', symbol: '$' },
-    { name: 'EUR', symbol: '€' },
-  ]
-    
-  //Submit the 'add expense' dialog content:
-  const submitExpense = async () => {
-    if (!isFormValid.value) return
-
-    //DEBUG: console.log('Submitted Data before:', formData.value)
-
-
-    // Add logic to submit the data via your API
-    formData.value.tripId = selectedTrip.value.id
-    formData.value.amount = parseFloat(formData.value.amount)
-
-    //DEBUG: console.log('Submitted Data after:', formData.value)
-    
-    // Send data to API
-    try {
-      await $fetch('/api/expenses', {
-        method: 'POST',
-        body: formData.value,
-      })
-
-      // Reset the form
-      resetForm()
-      // update the filtered Expenses List and set the cookie
-      // in order to display the added row.
-      tripChanged()
-
-      // close the Dialog form
-      isDialogOpen.value = false
-    } catch (error) {
-      console.error('Error submitting form:', error)
-      alert(error)
-    }
-  }
-
-  // Close Dialog without Submission of data
-  const closeDialog = () => {
-    resetForm()
-    isDialogOpen.value = false
-  }
 
   // fetch filtered Expenses
   const tripChanged = async () => {

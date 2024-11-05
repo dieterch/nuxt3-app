6084f060ea416dd93e3d4e011c914d23eca0edbd
dieterch <dieter.chvatal@gmail.com>
2024-11-05 17:43:13 +0000

    Test.vue update

--- pages/test.vue
+++ pages/test.vue
@@ -1,16 +1,22 @@
 <template>
 <v-container>
     <v-row>
-        <v-btn 
-            @click="cdtest1">
-            Create Dialog 1
-        </v-btn>
-        <v-btn @click="cdtest2">
-            Create Dialog 2
-        </v-btn>
-        <v-btn @click="cdtest3">
-            Create Dialog 3
-        </v-btn>
+        <v-col>
+            <v-btn 
+                @click="cdtest1">
+                Dialog 1
+            </v-btn>
+        </v-col>
+        <v-col>
+            <v-btn @click="cdtest2">
+                Dialog 2
+            </v-btn>
+        </v-col>
+        <v-col>
+            <v-btn @click="cdtest3">
+                Dialog 3
+            </v-btn>
+        </v-col>
     </v-row>
 </v-container>
 </template>
@@ -35,33 +41,33 @@ import { createDialog, warnDialog, confirmDialog } from 'vuetify3-dialog'
             buttons: [
                 { title: 'Ok', key: 'ok', /* any v-btn api option */ },
                 { title: 'Cancel', key: 'cancel', /* any v-btn api option */ },
-            ]}
-        )
+            ]
+        })
         console.log(answer)
     } 
 
     const cdtest2 = async () => {
 
-        const answer = await confirmDialog({ 
+        const answer = await createDialog({ 
             title: "Please Confirm", 
             text: `XX has 5 trips and 6 expenses. Continue to delete ?`,
             level: 'warning',
             icon: 'mdi-emoticon-happy-outline',
-            cancelText: 'Cancel',
-            confirmationText: 'Ok',
         })
         console.log(answer)
     }
 
     const cdtest3 = async () => {
 
-        const answer = await confirmDialog({ 
-            title: "Please Confirm", 
-            text: `XX has 5 trips and 6 expenses. Continue to delete ?`,
-            level: 'warning',
+        const answer = await createDialog({ 
+            title: "Edit in the Container", 
+            text: `Ist das lustig ?`,
+            level: 'error',
             icon: 'mdi-emoticon-happy-outline',
-            cancelText: 'Cancel',
-            confirmationText: 'Ok',
+            buttons: [
+                { title: 'Ok', key: 'ok', /* any v-btn api option */ },
+            ]
+
         })
         console.log(answer)
         }

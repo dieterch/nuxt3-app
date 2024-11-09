a24d77b1940ba8945ce66310ed63df885a6761b7
dieterch <dieter.chvatal@gmail.com>
2024-11-09 18:09:35 +0100

    expiration time extended

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -6,7 +6,7 @@ const JWT_SECRET = new TextEncoder().encode(config.public.JWT_SECRET)
 // const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET || 'your-default-secret-key')
 
 // Function to create a signed JWT
-export async function createToken(payload: JWTPayload, expiresIn = '1h'): Promise<string> {
+export async function createToken(payload: JWTPayload, expiresIn = '24h'): Promise<string> {
   return new SignJWT(payload)
     .setProtectedHeader({ alg: 'HS256' })
     .setExpirationTime(expiresIn)

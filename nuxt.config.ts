9878d8ec83d0d10dda9e19dd09dbe43ba23fa406
dieterch <dieter.chvatal@gmail.com>
2024-11-20 16:07:43 +0100

    runs on ios & web - no api access yet

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ import vuetify from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  ssr: false,
   devServer: {
     host: "0.0.0.0",
     port: 3003
